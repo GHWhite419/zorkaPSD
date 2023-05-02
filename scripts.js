d9ca6bbccd1336a5653d6ffaa58cff5ac9693471
Galen White <galenhwhite419@gmail.com>
2023-05-02 17:18:15 -0400

    Hamburger menu successfully styled

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
+const menuOpenButton = document.querySelector('.menuOpenButton');
 const menuCloseButton = document.querySelector('.menuCloseButton');
 const menu = document.querySelector('.settingsBanner');
+
+menuOpenButton.addEventListener('click', () => {
+    menu.classList.add('menuOpen');
+})
 
 menuCloseButton.addEventListener('click', () => {
     menu.classList.remove('menuOpen');
